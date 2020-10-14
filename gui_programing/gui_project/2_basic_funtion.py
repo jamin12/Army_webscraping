af914f1260d05d867917d9b720e7f3613b8efd8e
강경민 <rudals9901@naver.com>
2020-10-14 23:43:29 +0900

    .

--- gui_programing/gui_project/2_basic_funtion.py
+++ gui_programing/gui_project/2_basic_funtion.py
@@ -1,18 +1,57 @@
 from tkinter import *
 import tkinter.ttk as ttk
 import tkinter.messagebox as msgbox
+from tkinter import filedialog
 
 root = Tk()
 root.title("Nado Photo")
 root.resizable(False,False)
 
+# 파일 추가
+def add_file():
+    files = filedialog.askopenfilenames(title = "이미지 파일을 선택하세요", \
+                                        filetypes = (("JPG 파일","*.jpg"),("PNG 파일", "*.png"),("모든 파일","*.*")), \
+                                        initialdir = r"c:\desktop")
+    for file in files:
+        list_file.insert(END,file)
+
+# 선택 삭제
+def del_file():
+    for index in reversed(list_file.curselection()):
+        list_file.delete(index)
+
+# 저장 경로(폴더)
+def browse_dest_path():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected is None: #사용자가 취소를 누를 때
+        return
+    txt_dest_path.delete(0,END)
+    txt_dest_path.insert(0,folder_selected)
+
+#시작
+def start():
+    # 각 옵션들 값을 확인
+    print("가로넓이", cmb_width.get())
+    print("간격", cmb_space.get())
+    print("포맷", cmb_format.get())
+
+    # 파일 목록 화인
+    if list_file.size() == 0:
+        msgbox.showwarning("경고","이미지 파일을 추가하세요")
+        return
+    # 저장 경로 확인
+    if len(txt_dest_path.get()) == 0:
+        msgbox.showwarning("경고","저장 경로를 추가하세요")
+        return
+
+    
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
 file_frame.pack(fill = "x",padx = 5, pady = 5) #간격 띄우기
 
 #버튼 정의
-btn_add_file = Button(file_frame, text = "파일 추가",padx = 5, pady = 5, width = 12)
-btn_del_file = Button(file_frame, text = "선택 삭제",padx = 5, pady = 5, width = 12)
+btn_add_file = Button(file_frame, text = "파일 추가",padx = 5, pady = 5, width = 12,command = add_file)
+btn_del_file = Button(file_frame, text = "선택 삭제",padx = 5, pady = 5, width = 12,command = del_file)
 
 #버튼 프레임에 부착
 btn_add_file.pack(side = "left")
@@ -39,7 +78,7 @@ path_frame.pack(fill = "x",padx = 5, pady = 5,ipady = 5)
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side = "left", fill = "x", expand = True,padx = 5, pady = 5,ipady = 4)
 
-btn_dest_path = Button(path_frame,text = "찾아보기",width=10)
+btn_dest_path = Button(path_frame,text = "찾아보기",width=10,command = browse_dest_path)
 btn_dest_path.pack(side = "right",padx = 5, pady = 5)
 
 #옵션 프레임
@@ -96,7 +135,7 @@ run_frame.pack(fill = "x",padx = 5, pady = 5)
 btn_close = Button(run_frame, padx = 5,pady = 5,text = "닫기",width = 12,command = root.quit)
 btn_close.pack(side = "right",padx = 5, pady = 5)
 
-btn_start = Button(run_frame, padx = 5,pady = 5,text = "시작",width = 12)
+btn_start = Button(run_frame, padx = 5,pady = 5,text = "시작",width = 12,command = start)
 btn_start.pack(side = "right",padx = 5, pady = 5)
 
 
