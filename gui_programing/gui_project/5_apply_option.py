c0860376d55cf3277baad31959777edc00be622e
강경민 <rudals9901@naver.com>
2020-10-15 23:17:19 +0900

    .

--- gui_programing/gui_project/5_apply_option.py
+++ gui_programing/gui_project/5_apply_option.py
@@ -25,48 +25,85 @@ def del_file():
 # 저장 경로(폴더)
 def browse_dest_path():
     folder_selected = filedialog.askdirectory()
-    if folder_selected is None: #사용자가 취소를 누를 때
+    if folder_selected == '': #사용자가 취소를 누를 때
         return
     txt_dest_path.delete(0,END)
     txt_dest_path.insert(0,folder_selected)
 #이미지 통합
 def merge_image():
-    # print(list_file.get(0,END))# 모든 파일 목록을 가지고 오기
-    images = [Image.open(x) for x in list_file.get(0,END)]
-    #size -> size[0] : width, size[1] : height
-    # widths = [x.size[0] for x in images]
-    # heights = [x.size[1] for x in images]
+    # print("가로넓이", cmb_width.get())
+    # print("간격", cmb_space.get())
+    # print("포맷", cmb_format.get())
+    try:
+    #가로넓이 
+        img_width = cmb_width.get()
+        if img_width == "원본유지":
+            img_width = -1
+        else:
+            img_width = int(img_width)
 
-    widths, heights = zip(*(x.size[0] for x in images))
-    
-    #최대 넓이 ,전체 높이 구해옴
-    max_width, total_height = max(widths), sum(heights)
-    
-    #스케치북 준비
-    result_img = Image.new("RGB",(max_width,total_height),(255,255,255))
-    y_offset = 0 #y 위치
+        # 간격
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        elif img_space == "보통":
+            img_space = 60
+        elif img_space == "넓게":
+            img_space = 90
+        else:
+            img_space = 0
 
-    # for img in images:
-    #     result_img.paste(img,(0,y_offset))
-    #     y_offset += img.size[1]
-    
-    for idx, img in enumerate(images):
-        result_img.paste(img,(0,y_offset))
-        y_offset += img.size[1]
+        #포멧
+        img_format = cmb_format.get().lower()
+        ######################################################################
 
-        progress = (idx + 1) / len(images) * 100
-        p_var.set(progress)
-        progress_bar.update()
+        # print(list_file.get(0,END))# 모든 파일 목록을 가지고 오기
+        images = [Image.open(x) for x in list_file.get(0,END)]
 
-    dest_path = os.path.join(txt_dest_path.get(),"nado_photo.jpg")
-    result_img.save(dest_path)
-    msgbox.showinfo("알림", "작업이 완료 되었습니다.")
+        # 이미지 사이즈 리스트에 넣어서 하나씩 처리
+        image_sizes = []
+        if img_width > -1:
+            image_sizes = [(int(img_width), int(img_width * x.size[1]/x.size[0])) for x in images]
+        else:
+            image_sizes = [(x.size[0],x.size[1]) for x in images]
+
+        #size -> size[0] : width, size[1] : height
+        widths, heights = zip(*(image_sizes))
+        
+        #최대 넓이 ,전체 높이 구해옴
+        max_width, total_height = max(widths), sum(heights)
+        
+        #스케치북 준비
+        if img_space > 0: # 이미지 간격 옵션 적용
+            total_height += (img_space * (len(images) - 1))
+        result_img = Image.new("RGB",(max_width,total_height),(255,255,255))
+        y_offset = 0 #y 위치
+
+
+        for idx, img in enumerate(images):
+            # width 가 원본이 아닐 때에는 이미지 크기 조정
+            if img_width > -1:
+                img = img.resize(image_sizes[idx])
+            result_img.paste(img,(0,y_offset))
+            y_offset += (img.size[1] + img_space)
+
+            progress = (idx + 1) / len(images) * 100
+            p_var.set(progress)
+            progress_bar.update()
+
+        #포맷 옵션 처리
+        file_name = "nado_photo." + img_format
+        dest_path = os.path.join(txt_dest_path.get(),file_name)
+        result_img.save(dest_path)
+        msgbox.showinfo("알림", "작업이 완료 되었습니다.")
+    except Exception as err:
+        msgbox.showerror("에러",err)
 #시작 
 def start():
     # 각 옵션들 값을 확인
-    print("가로넓이", cmb_width.get())
-    print("간격", cmb_space.get())
-    print("포맷", cmb_format.get())
+    # print("가로넓이", cmb_width.get())
+    # print("간격", cmb_space.get())
+    # print("포맷", cmb_format.get())
 
     # 파일 목록 화인
     if list_file.size() == 0:
