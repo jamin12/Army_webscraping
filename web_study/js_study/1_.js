9ca62912782440fc6aafe56bda8acfff224e015b
강경민 <rudals9901@naver.com>
2020-11-03 23:35:38 +0900

    .

---
+++ web_study/js_study/1_.js
@@ -0,0 +1,4 @@
+const hihi = 123;
+
+// `을 사용해 ${변수} 하면 된다 
+console.log(`slslslsl ${hihi}`);
---
+++ web_study/js_study/3_change_type.js
@@ -0,0 +1,9 @@
+const mathScore = 100;
+const engScore = 200;
+const result =(mathScore + engScore) / 2;
+
+console.log(result)
+
+String(mathScore)
+Number("123")
+Boolean(0)
