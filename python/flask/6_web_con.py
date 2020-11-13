f7c3e306df5aa675e73b2383c2ccfec3c0543d9f
jamin12 <rudals9901@naver.com>
2020-11-13 23:03:04 +0900

    flask web connect success

--- python/flask/6_web_con.py
+++ python/flask/6_web_con.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/html_test')
+@app.route('/')
 def hello_html():
-    return render_template('simple.html')
+    return render_template('6_simple.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0" ,port="5500") 
+    app.run(debug=True) 
