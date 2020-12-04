66b6ac19be36e11c97441fa90258e119321e4808
jamin12 <rudals9901@naver.com>
2020-12-04 23:42:45 +0900

    .

--- python_algorithm_test/programmers.py
+++ python_algorithm_test/programmers.py
@@ -1,4 +1,7 @@
 import collections
+from itertools import combinations
+
+# 다리위를 지나는 트럭
 # def bridge_truck(bridge_length, weight, truck_weights):
 #     answer = 1
 #     t = collections.deque()
@@ -18,11 +21,39 @@ import collections
 #         if not deq and not truck_weights:
 #             return answer
 
-def stock_price(price):
-    
+# 주식가격
+# def stock_price(price):
+#     answer = []
+#     a = price[:]
+#     count = 0   
+#     for i in price:
+#         for ix,j in enumerate(a):
+#             if ix == 0:
+#                 continue
+#             if i <= j:
+#                 count = count + 1 
+#         a.pop(0)
+#         answer.append(count)
+#         count = 0
+#     print(price)
+#     return answer
 
-    return 0
+#두 개 뽑아서 더하기
+def two_number(number):
+    answer = []
+    #하나하나 더해보기
+    # for i in range(len(number)):
+    #     for j in range(i+1,len(number)):
+    #         answer.append(number[i] + number[j])
+    # answer = sorted(list(set(answer)))
 
+    #조합을 이용해 문제 풀이
+    a = list(combinations(number,2))
+    for i in a:
+        answer.append(i[0] + i[1])
+    answer = list(set(answer))
+    print(sorted(answer))
+    return answer.sort()
 
 if __name__ == "__main__":
     #####################################################################################################
@@ -32,6 +63,10 @@ if __name__ == "__main__":
     # bridge_truck(100,100,[10])
 
     # 주식 가격
-    stock_price([1, 2, 3, 2, 3])
+    # print(stock_price([1, 2, 3, 2, 3]))
+
+    #두 개 뽑아서 더하기
+    two_number([2,1,3,4,1])
+
 
 
